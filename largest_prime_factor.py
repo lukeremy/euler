77becad098d5b09975277dccdd2a4bd170ffc41f
Luke Remy <lnremy@gmail.com>
2016-12-12 09:25:20 -0800

    started new challenge

---
+++ largest_prime_factor.py
@@ -0,0 +1,22 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+num = 55
+
+largest_factor = 1
+factor_list = []
+for i in range(1,num):
+    if num % i == 0:
+        largest_factor = i
+        factor_list.append(i)
+
+print "largest factor: "
+print largest_factor
+print "factor_list: "
+print factor_list
+
+largest_prime
+
+for i in factor_list:
+    if i % range(1,i) != 0
