6863077362eb98a6f5762c978e57d97ca89bbf91
Luke Remy <lnremy@gmail.com>
2016-12-13 09:00:25 -0800

    worked on largest prime factor

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -2,21 +2,35 @@
 
 # What is the largest prime factor of the number 600851475143 ?
 
+import numpy as np
+
 num = 55
 
 largest_factor = 1
-factor_list = []
+factor_array = np.array([])
+
 for i in range(1,num):
     if num % i == 0:
         largest_factor = i
-        factor_list.append(i)
+        factor_array = np.r_[factor_array,largest_factor]
 
 print "largest factor: "
 print largest_factor
-print "factor_list: "
-print factor_list
+print "factor_array: "
+print factor_array
 
-largest_prime
 
-for i in factor_list:
-    if i % range(1,i) != 0
+def recursive_prime(array):
+    for x in range(2,array[-1]):
+
+        if array[-1] % x != 0:
+            largest_prime = array[-1]
+            return largest_prime
+        else:
+            array = array[0:-1]
+            recursive_prime(array)
+
+# largest_prime
+
+# for i in factor_list:
+#     if i % range(2,i) != 0
